a6f9e40008a40c5c4bfc5d6e7640c9660b14e687
Sulaiman Yahaya Adamu <sayahaya20@gmail.com>
2023-10-02 15:48:26 +0000

    #27 Bucket should not use random provider

--- providers.tf
+++ providers.tf
@@ -8,10 +8,7 @@ terraform {
 #   }
   
   required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
+   
    aws = {
       source = "hashicorp/aws"
       version = "5.18.1"
