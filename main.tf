249c1df71424bf4fa1cfd4013bdeeedeaa7ff986
Sulaiman Yahaya Adamu <104912127+sayahaya20@users.noreply.github.com>
2023-10-02 11:07:57 +0100

    #25 Organised root module files, added documentation to week 1 (#26)

--- main.tf
+++ main.tf
@@ -1,23 +1,4 @@
-terraform {
-  cloud {
-    organization = "Sulaiman_Yahaya_Cloud"
 
-    workspaces {
-      name = "Terra-house"
-    }
-  }
-  
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-   aws = {
-      source = "hashicorp/aws"
-      version = "5.18.1"
-    }
-  }
-}
 
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
@@ -33,9 +14,9 @@ resource "aws_s3_bucket" "example" {
   #Bucket Naming Rules 
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result
+  
+  tags = {
+    UserUid = var.user_uuid
+  }
+
 }
-
-
- output "random_bucket_name" {
-   value = random_string.bucket_name.result
- }
