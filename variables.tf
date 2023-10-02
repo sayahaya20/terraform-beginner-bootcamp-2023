784bc88bb04c32685251ec50a0aaee1f149c5de0
Sulaiman Yahaya Adamu <104912127+sayahaya20@users.noreply.github.com>
2023-10-02 16:51:06 +0100

    #27 Bucket should not use random provider (#28)

--- variables.tf
+++ variables.tf
@@ -8,4 +8,13 @@ variable "user_uuid" {
   }
 }
 
+variable "bucket_name" {
+  description = "The name of our s3 bucket"
+  type = string
 
+  validation {
+    condition = can(regex("^[a-z0-9.-]{3,63}$", var.bucket_name))
+    error_message = "Invalid S3 bucket name. Must be between 3 and 63 characters, containing only lowercase letters, numbers, hyphens, and dots."
+  }
+  
+}
