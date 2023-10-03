8490a2b38afaf42d28d9a0b64b4278e3d130425d
Sulaiman Yahaya Adamu <sayahaya20@gmail.com>
2023-10-03 23:31:44 +0000

    #31 Enabled static website hosting, uploaded index and error pages via terraform

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,10 @@
     description = "bucket name for static website"
     value = module.terrahouse_aws.bucket_name
  }
+
+
+ output "s3_website_endpoint" {
+  description = "s3 static website endpoint"
+  value =module.terrahouse_aws.website_endpoint
+   
+ }
