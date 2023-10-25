4bc9e01bc8897920045557a360d9cae9ae1a1721
Sulaiman Yahaya Adamu <104912127+sayahaya20@users.noreply.github.com>
2023-10-25 11:31:29 +0100

    45 terratowns provider (#47)
    * #45 wip settiong up the skeleton for creating a custom terraform provider
    
    * #45 finish implementing terraform provider

--- main.tf
+++ main.tf
@@ -1,21 +1,79 @@
 terraform {
-#   cloud {
-#     organization = "Sulaiman_Yahaya_Cloud"
+    required_providers {
+    terratowns = {
+      source = "local.providers/local/terratowns"
+      version = "1.0.0"
+    }
+  }
+  #backend "remote" {
+  #  hostname = "app.terraform.io"
+  #  organization = "ExamPro"
 
-#     workspaces {
-#       name = "Terra-house"
-#     }
-#   }
+  #  workspaces {
+  #    name = "terra-house-1"
+  #  }
+  #}
+  cloud {
+    organization = "Sulaiman_Yahaya_Cloud"
+    workspaces {
+      name = "Terra-house"
+    }
+  }
   
-
 }
 
-module "terrahouse_aws" {
+# module "terrahouse_aws" {
+#   source = "./modules/terrahome_aws"
+#   user_uuid = var.user_uuid
+#   bucket_name = var.bucket_name
+#   index_html_filepath = var.index_html_filepath
+#   error_html_filepath = var.error_html_filepath
+#   assets_path = var.assets_path
+#   content_version= var.content_version
+# }
+
+provider "terratowns" {
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
+}
+
+module "home_arcanum_hosting" {
   source = "./modules/terrahome_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
-  index_html_filepath = var.index_html_filepath
-  error_html_filepath = var.error_html_filepath
-  assets_path = var.assets_path
-  content_version= var.content_version
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.arcanum.public_path
+  content_version = var.arcanum.content_version
+}
+
+resource "terratowns_home" "home" {
+  name = "How to play Arcanum in 2023!"
+  description = <<DESCRIPTION
+Arcanum is a game from 2001 that shipped with alot of bugs.
+Modders have removed all the originals making this game really fun
+to play (despite that old look graphics). This is my guide that will
+show you how to play arcanum without spoiling the plot.
+
+DESCRIPTION
+  domain_name = module.home_arcanum_hosting.domain_name
+  town = "missingo"
+  content_version = var.arcanum.content_version
+}
+
+module "home_payday_hosting" {
+  source = "./modules/terrahome_aws"
+  user_uuid = var.teacherseat_user_uuid
+  public_path = var.payday.public_path
+  content_version = var.payday.content_version
+}
+
+resource "terratowns_home" "home_payday" {
+  name = "Making your Payday Bar"
+  description = <<DESCRIPTION
+Since I really like Payday candy bars but they cost so much to import
+into Canada, I decided I would see how I could my own Paydays bars,
+and if they are most cost effective.
+DESCRIPTION
+  domain_name = module.home_payday_hosting.domain_name
+  town = "missingo"
+  content_version = var.payday.content_version
 }
